4b5cf3e00dc438095270c8494c3af9b89f01c71d
sngrzdv <gruzdovasonya@gmail.com>
2025-06-25 00:31:45 +0300

    Добавьте файлы проекта.

---
+++ kursach/Pages/Autoriz.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kursach.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Autoriz.xaml
+    /// </summary>
+    public partial class Autoriz : Page
+    {
+        public Autoriz()
+        {
+            InitializeComponent();
+        }
+    }
+}
